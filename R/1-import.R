3ce93e0ccb9bb52eecabb75d1573e2a2db686b70
EPdeJ <erikpieterdejong@gmail.com>
2025-07-11 15:25:50 +0800

    commented some lines

--- R/1-import.R
+++ R/1-import.R
@@ -180,58 +180,58 @@ for(link in data_links){
     # !!! the open meteo package also has location based weather data 
     # see: https://open-meteo.com/en/docs/air-quality-api
     
-  # Download daily data
-    weather_history_taipei_daily <-  weather_history(location = "Taipei",
-                                                      start = "2014-01-01",
-                                                      end = "2024-12-31", 
-                                                      daily = list("weather_code",
-                                                                   "temperature_2m_mean",
-                                                                   "temperature_2m_max",
-                                                                   "temperature_2m_min",
-                                                                   "apparent_temperature_mean",
-                                                                   "apparent_temperature_max", 
-                                                                   "apparent_temperature_min",
-                                                                   "sunrise",
-                                                                   "sunset",
-                                                                   "daylight_duration",
-                                                                   "sunshine_duration",
-                                                                   "precipitation_sum",
-                                                                   "precipitation_hours",
-                                                                   "wind_speed_10m_max",
-                                                                   "wind_gusts_10m_max",
-                                                                   "wind_direction_10m_dominant")
-                                                      )
-  
-  # Download hourly data
-    weather_history_taipei_hourly <-  weather_history(location = "Taipei",
-                                                      start = "2014-01-01",
-                                                      end = "2024-12-31", 
-                                                      hourly = list("temperature_2m",
-                                                                    "precipitation",
-                                                                    "windspeed_10m", 
-                                                                    "cloudcover",
-                                                                    "apparent_temperature",
-                                                                    "weather_code",
-                                                                    "wind_direction_10m",
-                                                                    "wind_gusts_10m")
-                                                      )
-  # Save both datasets
-    fwrite(weather_history_taipei_daily, "data/weather_history_taipei_daily.csv",
-           sep = ",",                 
-           quote = TRUE,              
-           na = "",                   
-           bom = TRUE,                
-           row.names = FALSE,         
-           dateTimeAs = "ISO",       
-           logical01 = FALSE)
-    fwrite(weather_history_taipei_hourly, "data/weather_history_taipei_hourly.csv",
-           sep = ",",                 
-           quote = TRUE,              
-           na = "",                   
-           bom = TRUE,                
-           row.names = FALSE,         
-           dateTimeAs = "ISO",       
-           logical01 = FALSE) 
+  # # Download daily data
+  #   weather_history_taipei_daily <-  weather_history(location = "Taipei",
+  #                                                     start = "2014-01-01",
+  #                                                     end = "2024-12-31", 
+  #                                                     daily = list("weather_code",
+  #                                                                  "temperature_2m_mean",
+  #                                                                  "temperature_2m_max",
+  #                                                                  "temperature_2m_min",
+  #                                                                  "apparent_temperature_mean",
+  #                                                                  "apparent_temperature_max", 
+  #                                                                  "apparent_temperature_min",
+  #                                                                  "sunrise",
+  #                                                                  "sunset",
+  #                                                                  "daylight_duration",
+  #                                                                  "sunshine_duration",
+  #                                                                  "precipitation_sum",
+  #                                                                  "precipitation_hours",
+  #                                                                  "wind_speed_10m_max",
+  #                                                                  "wind_gusts_10m_max",
+  #                                                                  "wind_direction_10m_dominant")
+  #                                                     )
+  # 
+  # # Download hourly data
+  #   weather_history_taipei_hourly <-  weather_history(location = "Taipei",
+  #                                                     start = "2014-01-01",
+  #                                                     end = "2024-12-31", 
+  #                                                     hourly = list("temperature_2m",
+  #                                                                   "precipitation",
+  #                                                                   "windspeed_10m", 
+  #                                                                   "cloudcover",
+  #                                                                   "apparent_temperature",
+  #                                                                   "weather_code",
+  #                                                                   "wind_direction_10m",
+  #                                                                   "wind_gusts_10m")
+  #                                                     )
+  # # Save both datasets
+  #   fwrite(weather_history_taipei_daily, "data/weather_history_taipei_daily.csv",
+  #          sep = ",",                 
+  #          quote = TRUE,              
+  #          na = "",                   
+  #          bom = TRUE,                
+  #          row.names = FALSE,         
+  #          dateTimeAs = "ISO",       
+  #          logical01 = FALSE)
+  #   fwrite(weather_history_taipei_hourly, "data/weather_history_taipei_hourly.csv",
+  #          sep = ",",                 
+  #          quote = TRUE,              
+  #          na = "",                   
+  #          bom = TRUE,                
+  #          row.names = FALSE,         
+  #          dateTimeAs = "ISO",       
+  #          logical01 = FALSE) 
 
   # load both in environment
     weather_history_taipei_daily <- fread("data/weather_history_taipei_daily.csv")
