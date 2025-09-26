69479f3d71d92d0d6511af537cd0c5fcd90eb615
Russ <73149125+Russell-Shean@users.noreply.github.com>
2025-09-26 16:35:02 +0800

    finish combining old and new formats

--- R/charts/accidents_by_village.R
+++ R/charts/accidents_by_village.R
@@ -1,9 +1,17 @@
+library(forcats)
+library(ggplot2)
+library(scales)
+
+
+
+
 taiwan_historical2 |> 
   count(縣市_區鄉鎮) |>
-  filter(n > 20000) |>
+  filter(n > 100000) |>
   mutate(縣市_區鄉鎮 = fct_reorder(縣市_區鄉鎮, n, .desc = TRUE)) |> 
   ggplot() + geom_col(aes(x = 縣市_區鄉鎮, y = n)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+ 
+  scale_y_continuous(labels = label_comma())
 
 
 taiwan_historical2 |> 
@@ -17,7 +25,7 @@ taiwan_historical2 |>
 
 
 taiwan_historical2 |> 
-  filter(縣市_區鄉鎮 == "臺中市西屯區") |>
+  filter(縣市_區鄉鎮 == "桃園市中壢區") |>
   count(發生年度) |>
   
   #mutate(縣市_區鄉鎮 = fct_reorder(縣市_區鄉鎮, n, .desc = TRUE)) |> 
--- R/get_data/taiwan_historical/02-clean_data.R
+++ R/get_data/taiwan_historical/02-clean_data.R
@@ -56,7 +56,9 @@ old_format <- taiwan_historical |>
     
     # create columns that exist in new_format 
     # so that we can fix them together after left join
-    發生日期 = paste0(發生年度, "-", 發生月份, "-", 發生日)
+    發生日期 = paste0(發生年度, "-", 發生月份, "-", 發生日),
+    
+    發生時間 = paste0(發生小時, ":", 發生分鐘, ":", 發生分秒)
     
     
     )  |>
@@ -69,14 +71,18 @@ old_format <- taiwan_historical |>
   
   # create a party number value
   group_by(row_id) %>%
-  mutate(當事者順位 = row_number()) %>%  
+  mutate(當事者順位 = as.character(row_number())) %>%  
   
   # remove the extra row_id value
   ungroup() %>%
-  select(-row_id) 
+  select(-row_id) |>
+  
+  # separate out the vehicle category and subcategory 
+  mutate(`當事者區分.類別.大類別名稱.車種` = str_extract(車種, "(?<=-).*" ),
+         `當事者區分.類別.子類別名稱.車種` = str_extract(車種, ".*(?=-)"))
 
 
-old_format_subset |> tidyr::separate_rows(車種, sep = ";") |> View()
+#old_format_subset |> tidyr::separate_rows(車種, sep = ";") |> View()
 
 
 new_format <- taiwan_historical |> 
@@ -89,19 +95,50 @@ new_format <- taiwan_historical |>
     # combine date pieces into one coherent measurement
     發生日期 = paste0(發生年度, "-", 發生月份, "-", 發生日),
     
+    
+    # create an extra time column so we can compare new values to the originals
+    發生時間2 = 發生時間, 
+    
     # Fix (some of the time values) time so that there are leading zeros
     發生時間 = ifelse(nchar(發生時間) == 6,
                   發生時間, 
                   ifelse(nchar(發生時間) == 5,
                          paste0("0", 發生時間),
                          發生時間)
-                  )
+                  ),
+    
+    
+    # create hours, minutes and seconds from incident times with 6 digits
+    # Hour
+    發生小時 = ifelse(nchar(發生時間) == 6, 
+                  str_sub(發生時間, 1,2),
+                  NA),
+    
+    # minute
+    發生分鐘 = ifelse(nchar(發生時間) == 6, 
+                  str_sub(發生時間, 3,4),
+                  NA),
+    
+    # second
+    發生分秒 = ifelse(nchar(發生時間) == 6, 
+                  str_sub(發生時間, 5,6),
+                  NA),
+    
+    # redo the time column to include colons to separate numbers
+    # Do it only for 6 character values
+    # make everything else NA
+    # because who knows what time 0 or 1 are
+    發生時間 = ifelse(nchar(發生時間) == 6,
+                  paste0(發生小時, ":", 發生分鐘, ":", 發生分秒),
+                  NA)
+
   )
 
 
 
 
-taiwan_historical2 <- taiwan_historical  |> 
+taiwan_historical2 <- new_format  |> 
+  bind_rows(old_format) |> 
   
   
   # start creating new columns 
